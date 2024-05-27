72bc1daea3a1ea71ec09b650ccf8be51cd92defd
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-05-27 13:37:41 +0100

    Add godot_rl_agents-main folder

--- RingPong-starter/RingPong-ringpong_starter/Player.gd
+++ RingPong-starter/RingPong-ringpong_starter/Player.gd
@@ -2,20 +2,27 @@ extends Node3D
 
 @export var rotation_speed = 3.0
 @onready var ball = get_node("../Ball")
-var needs_reset = false
+@onready var ai_controller = $AIController3D
+
+func _ready():
+	ai_controller.init(self)
 
 func game_over():
-	needs_reset = true
+	ai_controller.done = true
+	ai_controller.needs_reset = true
 
 func _physics_process(delta):
-	if needs_reset:
+	if ai_controller.needs_reset:
+		ai_controller.reset()
 		ball.reset()
-		needs_reset = false
 		return
 		
-	var movement = Input.get_axis("rotate_anticlockwise", "rotate_clockwise")
+	var movement : float
+	if ai_controller.heuristic == "human":
+		movement = Input.get_axis("rotate_anticlockwise", "rotate_clockwise")
+	else:
+		movement = ai_controller.move_action
 	rotate_y(movement*delta*rotation_speed)
 
-
 func _on_area_3d_body_entered(body):
-	print("ball hit paddle")
+	ai_controller.reward += 1.0
--- RingPong-starter/RingPong-ringpong_starter/controller.gd
+++ RingPong-starter/RingPong-ringpong_starter/controller.gd
@@ -1,11 +1,26 @@
 extends AIController3D
 
+# Stores the action sampled for the agent's policy, running in python
+var move_action : float = 0.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func get_obs() -> Dictionary:
+	# get the balls position and velocity in the paddle's frame of reference
+	var ball_pos = to_local(_player.ball.global_position)
+	var ball_vel = to_local(_player.ball.linear_velocity)
+	var obs = [ball_pos.x, ball_pos.z, ball_vel.x/10.0, ball_vel.z/10.0]
 
+	return {"obs":obs}
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func get_reward() -> float:	
+	return reward
+	
+func get_action_space() -> Dictionary:
+	return {
+		"move_action" : {
+			"size": 1,
+			"action_type": "continuous"
+		},
+		}
+	
+func set_action(action) -> void:	
+	move_action = clamp(action["move_action"][0], -1.0, 1.0)
